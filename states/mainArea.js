4b232c3fa14391f5994413afc51aad2a22e533ff
Will <phifda@gmail.com>
2015-12-03 22:42:36 -0500

    selection cursor

--- states/mainArea.js
+++ states/mainArea.js
@@ -5,7 +5,8 @@ MainArea.prototype = {
         // menu background
         game.add.tileSprite(0, 0, game.width, game.height, 'grassTile')
 
-        this.placingGrid = game.add.tileSprite(0, 0, game.width, game.height, 'placingGrid')
+        // this.placingGrid = game.add.tileSprite(0, 0, game.width, game.height, 'placingGrid')
+        this.selection = game.add.sprite(0, 0, 'currentGrid')
 
         // gradient screen cover
         game.add.image(0, 0, 'cover')
@@ -13,6 +14,14 @@ MainArea.prototype = {
         // make it not auto-pause when you unfocus it
         // game.stage.disableVisibilityChange = true;
 
+        space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        space.onDown.addOnce(function() {this.selection.scale.setTo(2, 2)})
+
         Fade.fadeOut()
+    },
+
+    update: function() {
+        this.selection.x = game.input.x - (game.input.x % 32)
+        this.selection.y = game.input.y - (game.input.y % 32)
     }
 }
